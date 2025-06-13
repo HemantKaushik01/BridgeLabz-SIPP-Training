12aec1798bcde63f032c2b9d63778ea389098037
Dhruv Sahu <dhruvsahu7355@gmail.com>
2025-06-13 20:48:43 +0530

    {Hemant Kaushik} add. Factorial

---
+++ Control_flow/Factorial.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class Factorial{
+	public static void main(String[] args){
+		Scanner input = new Scanner(System.in);
+		int multi=1;
+		System.out.print("Enter a numbers by the user : ");
+		int number =input.nextInt();
+		for(int i=number;i>=1;i--){
+			multi*=i;
+		}
+		
+		System.out.print("factorial of the number" + number + " is "+ multi);			
+			
+	}
+}
