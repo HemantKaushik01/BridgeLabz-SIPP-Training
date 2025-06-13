bd537fb720ffff2e3c5021f92774af3ce35ac9c0
Dhruv Sahu <dhruvsahu7355@gmail.com>
2025-06-13 09:59:07 +0530

    {Hemant Kaushik} add. Counter for Rocket Launch by while loop

---
+++ Control_flow/CountDownByWhile.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class CountDownByWhile{
+	public static void main(String[] args){
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter a count down numbers : ");
+		int counter = input.nextInt();
+		while(counter>0){
+			System.out.println(counter--);
+		
+		}
+					
+	}
+}
