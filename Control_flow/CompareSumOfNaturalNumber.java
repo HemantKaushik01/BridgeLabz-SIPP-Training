9e1120cfcccd835b146a6c3926a42b0fd4e0349b
Dhruv Sahu <dhruvsahu7355@gmail.com>
2025-06-13 10:11:34 +0530

    {Hemant Kaushik} add. Is sum of natural numbers are same by formula and forloop

---
+++ Control_flow/CompareSumOfNaturalNumber.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+class CompareSumOfNaturalNumber{
+	public static void main(String[] args){
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter a numbers : ");
+		int number = input.nextInt();
+		
+		if(number>0){
+			int sumByFormula=number*(number+1)/2;
+			int sumByForLoop=0;
+			for(int i=1;i<=number;i++){
+				sumByForLoop+=i;
+			}
+			if(sumByFormula == sumByForLoop)
+			System.out.print( "The sum of " + sumByForLoop +" natural numbers is : " +number +" Comparison is true");
+
+			
+				
+			
+		}
+		else{
+			System.out.print( "The number "+number+" is not a natural number");
+		}
+		
+	}
+}
