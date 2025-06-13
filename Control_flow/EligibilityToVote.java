ec3cf134417cf37b1e0ff2efcff7b661ec361678
Dhruv Sahu <dhruvsahu7355@gmail.com>
2025-06-13 10:27:21 +0530

    {Hemant Kaushik} add. Check Eligibility to vote

---
+++ Control_flow/EligibilityToVote.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class EligibilityToVote{
+	public static void main(String[] args){
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter age of the person : ");
+		int age=input.nextInt();
+		if(age>=18){
+			System.out.print("The person age is " + age + " and can vote.");
+		}
+		else{
+			
+			System.out.print("The person age is " + age + " and cannot vote.");
+		}
+
+		
+	}
+}
