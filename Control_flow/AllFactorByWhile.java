d177cd5fd14f82dcfb23cefeeb8f981240a3fdac
Dhruv Sahu <dhruvsahu7355@gmail.com>
2025-06-14 10:14:56 +0530

    {Hemant Kaushik} add. Print all factor of a number using while loop

---
+++ Control_flow/AllFactorByWhile.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class AllFactorByWhile{
+	public static void main(String[] args){
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter a number for factor: ");
+		int number = input.nextInt();
+		int counter = number-1;
+		while(counter>0){
+			if(number%counter==0){
+				System.out.print(counter +" ");
+			}
+			counter--;
+		}			
+			
+	}
+}
