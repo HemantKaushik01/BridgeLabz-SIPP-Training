9f7d1201bc9affc37dfc4e2405ed5c839ef3278f
Dhruv Sahu <dhruvsahu7355@gmail.com>
2025-06-14 09:43:27 +0530

    {Hemant Kaushik} add. Employee Bonus

---
+++ Control_flow/EmployeeBonus.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class EmployeeBonus {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter employee salary: ");
+        double salary = scanner.nextDouble();
+
+
+        System.out.print("Enter years of service: ");
+        int yearsOfService = scanner.nextInt();
+
+        if (yearsOfService > 5) {
+            double bonus = 0.05 * salary;
+            System.out.println("Bonus amount: ₹" + bonus);
+        } else {
+            System.out.println("No bonus. Years of service is less than or equal to 5.");
+        }
+    }
+}
