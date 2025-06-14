c1bdbb61ff92b0705ca151d11674fa78a2b2ab2b
Dhruv Sahu <dhruvsahu7355@gmail.com>
2025-06-14 09:51:29 +0530

    {Hemant Kaushik} add. Print odd even till number

---
+++ Control_flow/OddEven.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class OddEven {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number =input.nextInt();
+	for(int i=1;i<=number;i++){
+		if(i%2==0){
+			System.out.println("The number is even : "+i);
+		}
+		else{
+			System.out.println("The number is odd : "+i);
+		}
+	}
+    }
+}
