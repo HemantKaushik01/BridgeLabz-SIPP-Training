9b352c021ae00498bfaacf19ca390ce7a76eae9a
Dhruv Sahu <dhruvsahu7355@gmail.com>
2025-06-16 09:43:03 +0530

    {Hemant Kaushik} add. Print the Multiplication Table of a Number

---
+++ Arrays/MultiplicationTable.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class MultiplicationTable{
+	public static void main(String[] args){
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter the number : "); 
+		int number = input.nextInt();
+		int arr[]=new int[10];
+		for(int i=1;i<=10;i++){
+			arr[i-1]=number*i;
+		}
+		System.out.println("Print the multiplication array : ");
+		for(int i=0;i<10;i++){
+			System.out.println(arr[i]);
+		}
+	}
+}
