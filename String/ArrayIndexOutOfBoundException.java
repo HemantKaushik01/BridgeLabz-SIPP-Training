e17582b1d7acab451f07b42c78d8bc9b1291d11f
Dhruv Sahu <dhruvsahu7355@gmail.com>
2025-06-19 09:35:26 +0530

    {Hemant Kaushik} add . Sub String using CharAt

--- String/ArrayIndexOutOfBoundException.java
+++ String/ArrayIndexOutOfBoundException.java
@@ -1,121 +1,36 @@
-package String;
-
-import java.util.ArrayList;
 import java.util.Scanner;
 
-class BookStore {
-    static Scanner input = new Scanner(System.in);
+public class ArrayIndexOutOfBoundException {
+
+       public static void handleException(String[] names) {
+        try {
+            System.out.println("\nHandling Exception:");
+            System.out.println("Accessing names[" + names.length + "]: " + names[names.length]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Caught ArrayIndexOutOfBoundsException: " + e.getMessage());
+        } catch (RuntimeException e) {
+            System.out.println("Caught RuntimeException: " + e.getMessage());
+        }
+    }
 
     public static void main(String[] args) {
-        ArrayList<String> title = new ArrayList<>();
-        ArrayList<String> author = new ArrayList<>();
-        ArrayList<Integer> price = new ArrayList<>();
+        Scanner input = new Scanner(System.in);
 
-        System.out.print("How many books do you want to enter initially? ");
-        int n = input.nextInt();
-
-        for (int i = 0; i < n; i++) {
-            System.out.println("Enter details for book " + (i + 1));
-            System.out.print("Title: ");
-            title.add(input.next());
-            System.out.print("Author: ");
-            author.add(input.next());
-            System.out.print("Price: ");
-            price.add(input.nextInt());
+        // Take number of names from user
+        System.out.print("Enter number of names: ");
+        int size = input.nextInt();
+        input.nextLine(); 
+        // Declare and take names as input
+        String[] names = new String[size];
+        for (int i = 0; i < size; i++) {
+            System.out.print("Enter name[" + i + "]: ");
+            names[i] = input.nextLine();
         }
 
-        System.out.println("\n--- Add New Book ---");
-        addNewBook(title, author, price);
+        // Step 1: Call method to generate unhandled exception (comment after observing)
+        // generateException(names); // Uncomment to see abrupt stop
 
-        System.out.println("\n--- Remove Book ---");
-        System.out.print("Enter title of book to remove: ");
-        String titleOfBook = input.next();
-        removeBook(title, author, price, titleOfBook);
-
-        System.out.println("\n--- Search Book ---");
-        System.out.print("Enter title of book to search: ");
-        String search = input.next();
-        searchBook(title, author, price, search);
-
-        System.out.println("\n--- Display All Books ---");
-        display(title, author, price);
-    }
-
-    public static void addNewBook(ArrayList<String> title, ArrayList<String> author, ArrayList<Integer> price) {
-        System.out.print("Enter title: ");
-        title.add(input.next());
-        System.out.print("Enter author: ");
-        author.add(input.next());
-        System.out.print("Enter price: ");
-        price.add(input.nextInt());
-
-        System.out.print("Do you want to add more books? (true/false): ");
-        if (input.nextBoolean()) {
-            addNewBook(title, author, price);
-        }
-    }
-
-    public static void removeBook(ArrayList<String> title, ArrayList<String> author, ArrayList<Integer> price, String titleOfBook) {
-        boolean found = false;
-        for (int i = 0; i < title.size(); i++) {
-            if (title.get(i).equals(titleOfBook)) {
-                title.remove(i);
-                author.remove(i);
-                price.remove(i);
-                System.out.println("Book removed successfully.");
-                found = true;
-                break;
-            }
-        }
-        if (!found) {
-            System.out.println("Book not found.");
-        }
-
-        System.out.print("Do you want to remove another book? (true/false): ");
-        if (input.nextBoolean()) {
-            System.out.print("Enter title of book to remove: ");
-            titleOfBook = input.next();
-            removeBook(title, author, price, titleOfBook);
-        }
-    }
-
-    public static void searchBook(ArrayList<String> title, ArrayList<String> author, ArrayList<Integer> price, String search) {
-        boolean found = false;
-        for (int i = 0; i < title.size(); i++) {
-            if (title.get(i).equals(search)) {
-                System.out.println("Book found:");
-                System.out.println("Title: " + title.get(i));
-                System.out.println("Author: " + author.get(i));
-                System.out.println("Price: " + price.get(i));
-                found = true;
-                break;
-            }
-        }
-        if (!found) {
-            System.out.println("Book not found.");
-        }
-
-        System.out.print("Do you want to search for another book? (true/false): ");
-        if (input.nextBoolean()) {
-            System.out.print("Enter title of book to search: ");
-            search = input.next();
-            searchBook(title, author, price, search);
-        }
-    }
-
-    public static void display(ArrayList<String> title, ArrayList<String> author, ArrayList<Integer> price) {
-        if (title.isEmpty()) {
-            System.out.println("No books available.");
-            return;
-        }
-
-        System.out.println("Book List:");
-        for (int i = 0; i < title.size(); i++) {
-            System.out.println("Book " + (i + 1) + ":");
-            System.out.println("Title: " + title.get(i));
-            System.out.println("Author: " + author.get(i));
-            System.out.println("Price: " + price.get(i));
-            System.out.println("---------------------------");
-        }
+        // Step 2: Call method to handle exception safely
+        handleException(names);
     }
 }
---
+++ String/SubStringUsingCharAt.java
@@ -0,0 +1,32 @@
+import java.util.*;
+class SubStringUsingCharAt{
+	public static void main(String[] args){
+		Scanner input =new Scanner(System.in);
+		System.out.print("Enter the value of String  : ");  
+		String str = input.next();
+		System.out.println("Enter the start and ending value of substring: ");
+		int start =input.nextInt();
+		int end = input.nextInt();
+		String strCharAt = substring(str, start, end);
+		// compare sub string
+		System.out.println("compare both sub string: " );
+		display(str,strCharAt, start, end);
+	}
+	public static String substring(String str, int start, int end){
+		 String newStr = "";
+		for(int i=start;i<end;i++){
+			newStr+=str.charAt(i);
+		}
+		return newStr;
+	}
+	public static void display (String str, String newStr, int  start , int end){	
+		str = str.substring(start,end);
+		System.out.println( "Both subString is  using charAt " + newStr + " and using substring() method " + str);
+		if(str.equals(newStr)){
+			System.out.print("true");
+		}
+		else{
+			System.out.print("false");
+		}
+	}
+}
