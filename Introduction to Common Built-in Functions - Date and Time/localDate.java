f8eabd842d164807644ce544bc23d41617c6aee4
Dhruv Sahu <dhruvsahu7355@gmail.com>
2025-06-19 11:30:39 +0530

    {Hemant Kaushik} add . Display Local Date by update some operations

--- Introduction to Common Built-in Functions - Date and Time/localDate.java
+++ Introduction to Common Built-in Functions - Date and Time/localDate.java
@@ -7,18 +7,17 @@ import java.util.*;
 public class localDate {
     public static void main(String[] args) {
 	Scanner input =new Scanner(System.in);
+	System.out.println("Enter date in (yyyy-MM-dd)  : ");
 	String dateString = input.next();
 
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 	LocalDate date = LocalDate.parse(dateString,formatter);
-	LocalDate dayUpdate = date.plusDays(07);
-        LocalDate monthsUpdate = date.plusMonths(01);
-	LocalDate yearsUpdate = date.plusYears(02);
-	LocalDate weeksUpdate = date.minusWeeks(03);
-	System.out.println("Day update in date by 7 days is : "+ dayUpdate);
-	System.out.println("Month update in date by 1 months is : "+ monthsUpdate);
-	System.out.println("Year update in date by 2 years is : "+ yearsUpdate);
-	System.out.println("Week update in date by minus 3 weeks is : "+ weeksUpdate);
+	date = date.plusDays(07);
+        date = date.plusMonths(01);
+	date = date.plusYears(02);
+	date = date.minusWeeks(03);
+	System.out.println("new updated date is : "+ date);
+	
 
 	
 	
