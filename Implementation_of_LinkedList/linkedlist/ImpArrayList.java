2e7459acf743e7400b77e4452b8e16b653e00d9c
Hemant Kaushik <hemantka69@gmail.com>
2025-07-21 11:06:54 +0530

    {Hemant Kaushik} add Read File

---
+++ Implementation_of_LinkedList/linkedlist/ImpArrayList.java
@@ -0,0 +1,14 @@
+package linkedlist;
+import java.util.*;
+public class ImpArrayList {
+		public static void main(String[] args) {
+			ArrayList<Integer> ll = new ArrayList<>();
+			ll.add("hello");
+			ll.add(13);
+			ll.add('c');
+			ll.add(false);
+			System.out.print(ll);
+			
+			
+		}
+}
