465c24a5a9aaeb7ed9d21e0fe1fee676c6358dd7
Hemant Kaushik <hemantka69@gmail.com>
2025-07-28 10:16:15 +0530

    {Hemant Kaushik} add Flight Scheduling System

--- Generics/SmartWarehouse.java
+++ Generics/SmartWarehouse.java
@@ -1,9 +1,18 @@
-
+class Gen<T>{
+	private T rollNo;
+	public Gen(T rollNo) {
+		this.rollNo = rollNo;
+	}
+	public T getGen() {
+		return rollNo;
+	}
+}
 public class SmartWarehouse {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.print("Smart Warehouse");
+		
+		Gen<Integer> first = new Gen<>(101);
+		System.out.print(first.getGen());
 
 	}
 
