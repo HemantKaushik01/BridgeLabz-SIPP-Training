26aef95212da89aa119edc07cddada1fea550f2f
Hemant Kaushik <hemantka69@gmail.com>
2025-07-30 14:24:16 +0530

    {Hemant Kaushik} add Sets are Equal

---
+++ Collections/listInterface/nthElement.java
@@ -0,0 +1,23 @@
+package listInterface;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class nthElement {
+
+	public static void main(String[] args) {
+		int [] arr= {1,33,33,33,2,3,5,4};
+		int n=4;
+		List<Integer> list = new LinkedList<>();
+		for(int i=0;i<arr.length;i++) {
+			list.add(arr[i]);
+			
+		}
+		
+		
+		
+		
+
+	}
+
+}
