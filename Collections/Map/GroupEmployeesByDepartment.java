7a1d5a0bb5232abfa09500e37c803884d368649c
Hemant Kaushik <hemantka69@gmail.com>
2025-08-04 11:37:50 +0530

    input Stream

--- Collections/Map/GroupEmployeesByDepartment.java
+++ Collections/Map/GroupEmployeesByDepartment.java
@@ -10,7 +10,7 @@ import java.util.List;
 class Employee {
     private String name;
     private String department;
-
+ 
     public Employee(String name, String department) {
         this.name = name;
         this.department = department;
