b8489c616f5ac7b2117eb471ad9cc48ed4474fe4
Hemant Kaushik <hemantka69@gmail.com>
2025-08-07 13:05:36 +0530

    {Hemant Kaushik} add Extract Currency Values

--- Regex&JUnit/regex/ExtractCurrencyValues.java
+++ Regex&JUnit/regex/ExtractCurrencyValues.java
@@ -4,16 +4,15 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 public class ExtractCurrencyValues {
 	public static void main(String[] args) {
-		 String regex = "\\b(\\w+)\\b\\s+\\1\\b"; 
+		 String regex = "\\$?\\d+(\\.\\d{2})?"; 
 	        Pattern pattern = Pattern.compile(regex);
-	        String input = "this this is is a repeated  repeated  word test.";
+	        String input = "The price is $45.99, and the discount is 10.50.";
 	        
 	        Matcher matcher = pattern.matcher(input);
 	        
 	        while (matcher.find()) {
-	            System.out.println("Matched: " + matcher.group(1));
+	            System.out.println("Matched: " + matcher.group());
 	        }
 
 	}
 }
-
