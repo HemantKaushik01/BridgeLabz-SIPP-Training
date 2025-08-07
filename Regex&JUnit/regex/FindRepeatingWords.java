8a4327ae85a03eee061c3e711693588d594086e7
Hemant Kaushik <hemantka69@gmail.com>
2025-08-07 13:11:37 +0530

    {Hemant Kaushik} add Find Repeating Words

--- Regex&JUnit/regex/FindRepeatingWords.java
+++ Regex&JUnit/regex/FindRepeatingWords.java
@@ -4,14 +4,21 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 public class FindRepeatingWords {
 	public static void main(String[] args) {
-		 String regex = "\\b(\\w+)\\b\\s+\\1\\b"; 
+		 String regex = "\\b\\d{3}-\\d{2}-\\d{4}\\b"; 
 	        Pattern pattern = Pattern.compile(regex);
-	        String input = "this this is is a repeated  repeated  word test.";
+	        String[] inputs = {
+	                "My SSN is 123-45-6789.",
+	                "This one is invalid: 123456789.",
+	                "Another wrong: 12-345-6789."
+	            };
 	        
-	        Matcher matcher = pattern.matcher(input);
-	        
-	        while (matcher.find()) {
-	            System.out.println("Matched: " + matcher.group(1));
+	        for (String text : inputs) {
+	            Matcher matcher = pattern.matcher(text);
+	            if (matcher.find()) {
+	                System.out.println( matcher.group() + " is valid");
+	            } else {
+	                System.out.println(" No valid SSN found in: \"" + text + "\"");
+	            }
 	        }
 
 	}
