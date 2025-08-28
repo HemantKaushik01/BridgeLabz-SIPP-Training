07261603437bd982b27f869dc946193f04d4d30e
Hemant Kaushik <hemantka69@gmail.com>
2025-08-28 10:16:52 +0530

    {Hemant Kaushik} add Smart_Device

--- Functional_Interface/Smart_Device.java
+++ Functional_Interface/Smart_Device.java
@@ -3,6 +3,9 @@ interface SmartDevice {
     void turnOff();
 }
 
+
+
+
 class Light implements SmartDevice {
     public void turnOn() { System.out.println("Light is ON"); }
     public void turnOff() { System.out.println("Light is OFF"); }
